d7749c7c41686c34bbfc3466afc44bed3ef463dd
syab-syab <syabsyabsyabsyab@gmail.com>
2025-02-22 21:48:22 +0900

    サイドパネルでエラー発生。配列だと上手く行かないっぽいかも

--- sidepanel.tsx
+++ sidepanel.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
+import { useStorage } from "@plasmohq/storage/hook"
 
 function IndexSidePanel() {
   const [data, setData] = useState("")
+  const [dataArray, setDataArray] = useStorage<Array<string>>("data-array", ["test1", "test2", "test3"])
 
   // indexedDBが使えないから
   // key(日本語OKならヨシ): [](ストックしたワードを文字列で配列にする)
@@ -17,6 +19,15 @@ function IndexSidePanel() {
         localStorageでPrivate Word Stockのような機能を実装してみる
       </h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
+      <ul>
+        {
+          dataArray.map((d) => {
+            return (
+              <li>{d}</li>
+            )
+          })
+        }
+      </ul>
     </div>
   )
 }
